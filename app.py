a43ae2489bdcd9b79462ed720e20c1682154cd80
dcfernandez1023 <54515508+dcfernandez1023@users.noreply.github.com>
2021-03-27 11:41:17 -0700

    emails work

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_cors import CORS
-import os
-from twilio.rest import Client
+from flask_mail import Mail, Message
+from datetime import date, datetime
+import pytz
+
 
 app = Flask(__name__)
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'dom.portfolio.bot@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'dom.portfolio.bot@gmail.com'
+app.config['MAIL_PASSWORD'] = 'thegreendoorhouse1'
+mail = Mail(app)
 CORS(app)
 
 
@@ -12,18 +21,31 @@ def index():
     return 'Portfolio Notifier'
 
 
-@app.route('/api/sendSms', methods=['POST'])
-def send_sms():
-    request_body = request.get_json()
-    # account_sid = os.environ['TWILIO_ACCOUNT_SID']
-    # auth_token = os.environ['TWILIO_AUTH_TOKEN']
-    # client = Client(account_sid, auth_token)
-    # message = client.messages.create(
-    #              body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-    #              from_='+16505347403',
-    #              to='+12488262294')
-    print(request_body)
-    return {"message": "success"}
+@app.route('/api/sendVisitEmail', methods=['POST'])
+def send_email():
+    request_body = dict(request.get_json())
+    pst = pytz.timezone('America/Los_Angeles')
+    today = date.today()
+    current_time = datetime.now(pst).strftime("%H:%M:%S")
+    message = Message("Your Portfolio was just Viewed on " + str(today),
+                      sender=app.config["MAIL_USERNAME"],
+                      recipients=["dominic.fernandez1023@gmail.com"])
+    ip_address = request_body.get("ip")
+    ip_version = request_body.get("version")
+    city = request_body.get("city")
+    region = request_body.get("region")
+    country = request_body.get("country_name")
+    body = "Portfolio was viewed on " + str(today) + " at " + str(current_time) + "\n"
+    body += "\nVisitor Details\n"
+    body += "-------------------------\n"
+    body += "# IP Address: " + str(ip_address) + "\n"
+    body += "# IP Version: " + str(ip_version) + "\n"
+    body += "# City: " + str(city) + "\n"
+    body += "# Region/State: " + str(region) + "\n"
+    body += "# Country: " + str(country) + "\n"
+    message.body = body
+    mail.send(message)
+    return make_response({}, 200)
 
 
 app.run(port=5000)
